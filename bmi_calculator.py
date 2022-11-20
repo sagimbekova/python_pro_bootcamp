7deefcb908e60fe9d366c2c5318d0af8286aaaac
sagimbekova <118532495+sagimbekova@users.noreply.github.com>
2022-11-20 16:10:10 +0600

    add print conditions

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,6 +1,16 @@
 
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 
-bmi_result = int(weight) / float(height) ** 2
-print(round(bmi_result))
+bmi_result = round(weight / height ** 2)
+
+if bmi_result <= 18.5:
+    print(f"Your BMI is {bmi_result}, you are underweight.")
+elif bmi_result > 18.5 and bmi_result <= 25:
+    print(f"Your BMI is {bmi_result}, you have a normal weight.")
+elif bmi_result > 25 and bmi_result <= 30:
+    print(f"Your BMI is {bmi_result}, you are slightly overweight.")
+elif bmi_result > 30 and bmi_result <= 35:
+    print(f"Your BMI is {bmi_result}, you are obese.")
+elif bmi_result > 35:
+    print(f"Your BMI is {bmi_result}, you are clinically obese.")
