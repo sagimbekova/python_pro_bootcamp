6cd68d095c8a3fedf76a2832fc3e7c8b1a727e2b
sagimbekova <118532495+sagimbekova@users.noreply.github.com>
2022-11-23 21:53:55 +0600

    add guessed loop

--- hangman.py
+++ hangman.py
@@ -1,18 +1,30 @@
-#Step 1 
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-print(chosen_word)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
+#TODO-3: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
+#TODO-4: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for position in range(word_length):
+    letter = chosen_word[position]
     if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = guess
+        
+
+print(display)
